b73426546f80e16c621fff3ebfd17489af8e632a
deperazar <danielpblog2014@gmail.com>
2022-02-13 14:15:28 -0500

    Added sound functionality

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,6 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import FactsList from '../components/FactsList';
 import CategorySelector from '../components/CategorySelector';
 import Scroll from '../components/Scroll';
-import Fact from '../components/Fact';
 import chuck from '../img/chuck.png';
 import 'animate.css';
 
@@ -16,6 +15,7 @@ class App extends Component {
             facts: [],
             categories: [],
             selectedCategory : '',
+            finishedPlaying: false,
             repeatedCount: 0,
             displayedAll: false
         }
@@ -43,29 +43,65 @@ class App extends Component {
         }        
     }
 
-    addFact = (category, count=0) => {
-        !category.length ? //If its an empty string
-            fetch('https://api.chucknorris.io/jokes/random')
-            .then(resp => resp.json())
-            .then(newFact => this.appendIfNotRepeated(category, newFact))        
-            .catch(error => console.log(error))
-        :
-        (
-            fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)  
-            .then(resp => resp.json())
-            .then(newFact => this.appendIfNotRepeated(category, newFact))        
-            .catch(error => console.log(error))
-        )
+    playFact = (text, onEndFunction = ()=>{}) => {
+        const options = {
+            key: 'ddb7eaed19ad4c66a911c6e73ccb9a19',
+            language: 'en-us',
+            voice: 'Mike',
+            audioFormat: '16khz_16bit_mono',
+            src: encodeURIComponent(text)
+        }
+        
+        fetch(`http://api.voicerss.org/?key=${options.key}&hl=${options.language}&v=${options.voice}&c=MP3&f=${options.audioFormat}&src=${options.src}`, {
+        "method": "GET"
+        })
+        .then(response => {
+            const audio = new Audio(response.url);
+            audio.play()
+            audio.addEventListener("ended", onEndFunction);
+        })
+        .catch(err => console.error(err));     
     }
 
+    async addFact (category) {
+        if (!category.length){
+            const responseFetch = await fetch('https://api.chucknorris.io/jokes/random');
+            const responseJson = await responseFetch.json();
+            this.appendIfNotRepeated(category, responseJson)
+            return responseJson;
+        } else {
+            const responseFetch = await fetch(`https://api.chucknorris.io/jokes/random?category=${category}`);
+            const responseJson = await responseFetch.json();
+            this.appendIfNotRepeated(category, responseJson)
+            return responseJson;
+        }
+    }
+
+    goAuto = () => {
+        const {selectedCategory, finishedPlaying} = this.state;
+        if (!finishedPlaying) {
+            this.addFact(selectedCategory)
+            .then(fact => this.playFact(fact.value, this.goAuto))
+            .then(console.log)
+        }
+    }
+    
+    stop = () => {
+        this.setState({finishedPlaying: true})
+    }
+
+    
     changeCategory = (category) => {
-        this.setState({selectedCategory: category})
+        category === 'All'?
+            this.setState({selectedCategory: ''})
+        :
+            this.setState({selectedCategory: category})
     }
 
     componentDidMount(){
         fetch('https://api.chucknorris.io/jokes/categories')  
         .then(resp => resp.json())
-        .then(categories => this.setState({categories: categories}))
+        .then(categories => this.setState({categories: ["All", ...categories]}))
         .catch(error => console.log(error));
     }
 
@@ -79,25 +115,36 @@ class App extends Component {
                     <p className='get-in'>Or if you already have an account <br /> <a>Sign In here</a></p>
                 </div> */}
                 <h1>Chuck Norris Fun Facts</h1>
-                <div className="dropdown">
-                <div className="dropbtn">
-                    <p>Categories</p> 
-                    <div className='arrow down'></div>
+                <div className='top-row'>
+                    <div className="warning-msg"><p>¡Beware! Some fun facts may be politically incorrect for certain audiences</p></div>
+                    <div className="dropdown">
+                        <div className="dropbtn">
+                           <p>Categories</p> 
+                           <div className='arrow down'></div>
+                        </div>
+                
+                        <div className="dropdown-content scrollWrapper">
+                        {
+                            categories.map((category, index) => {
+                                return (<CategorySelector key={index} category={category} changeCategory={this.changeCategory}/>)
+                            })
+                        }
+                        </div>
+                    </div>
+                    
                 </div>
-                <div className="dropdown-content scrollWrapper">
-                    {
-                        categories.map((category, index) => {
-                            return (<CategorySelector key={index} category={category} changeCategory={this.changeCategory}/>)
-                        })
-                    }
-                </div>
-
-                </div>
-                <p>¡Beware! Some fun facts may be politically incorrect for some audiences</p>
+                
                 <div className='body-structure'>
                     <div className='body-doc'>
                         <img className="image animate__animated animate__jackInTheBox" src={chuck} alt="Chuck"/>
-                        <button className='button cool-bttn' onClick={() => this.addFact(selectedCategory)}>Get a Random FunFact</button>
+                        <div style={{width: '50%'}}>
+                            <button className='button cool-bttn' onClick={() => this.addFact(selectedCategory)}>Get a Random FunFact</button>
+                            <div style={{display: 'flex', justifyContent: 'space-around', marginTop: '10px'}}>
+                                <button className='button cool-bttn' onClick={()=>this.goAuto()}> Automatic </button>
+                                <button className='button cool-bttn' onClick={()=>this.stop()}> Stop </button>
+                            </div>
+                        </div>
+                        
                     </div>
                         <Scroll>
                             {!facts.length ? 
@@ -105,6 +152,7 @@ class App extends Component {
                             : (
                                 <ErrorBoundary>
                                             <FactsList facts={facts} displayedAll={displayedAll}/>
+                                            
                                 </ErrorBoundary>
                             )}
                         </Scroll>
